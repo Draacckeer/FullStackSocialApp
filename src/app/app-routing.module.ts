7ff25a3a7fb282c36be56ef0ecddb7d81ee5738e
Draacckeer <u201922975@upc.edu.pe>
2022-07-05 22:14:20 -0500

    Add: Home Responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,5 +4,5 @@ export const appRoutes: Route[] = [
 
   { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
   { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule)},
-  
+
 ];
