1d52dea80071a26f14a673b1add64832a78722fb
Draacckeer <u201922975@upc.edu.pe>
2022-07-15 22:54:18 -0500

    Add: User Display in Navbar

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {UsersService} from "../../services/users.service";
+import {UserPublication} from "../../models/userPublication";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: 'navbar.component.html'
 })
 
-export class NavbarComponent{
-  constructor(private route: Router) {
+export class NavbarComponent implements OnInit {
+  user: UserPublication = {} as UserPublication;
+
+  constructor(private route: Router , private usersService: UsersService) {
+  }
+
+  ngOnInit() {
+    this.usersService.getUserPublicationByToken().subscribe({
+      next: (response: UserPublication) => {
+        this.user = response;
+      }
+    })
   }
 
   logout(){
