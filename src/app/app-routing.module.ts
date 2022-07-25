c6adddaea5c6d570773f29fd05cd7321467772e0
Draacckeer <u201922975@upc.edu.pe>
2022-07-24 23:53:54 -0500

    Add: Routing Redirect to Auth Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import {Route} from '@angular/router';
+import {NoAuthGuard} from "./auth/guards/no-auth.guard";
+import {AuthGuard} from "./auth/guards/auth.guard";
 
 export const appRoutes: Route[] = [
 
-  { path: '', loadChildren: () => import('./modules/auth/home/home.module').then(m => m.HomeModule)},
-  { path: 'register', loadChildren: () => import('./modules/auth/register/register.module').then(m => m.RegisterModule)},
-  { path: 'publications', loadChildren: () => import('./modules/admin/pages/publications/publications.module').then(m => m.PublicationsModule)},
-  { path: 'profile', loadChildren: () => import('./modules/admin/pages/profile/profile.module').then(m => m.ProfileModule)},
-  { path: 'meet', loadChildren: () => import('./modules/admin/pages/meet/meet.module').then(m => m.MeetModule)},
-  { path: 'messages', loadChildren: () => import('./modules/admin/pages/messages/messages.module').then(m => m.MessagesModule)},
+  {
+    path: '',
+    canActivate: [NoAuthGuard],
+    canActivateChild: [NoAuthGuard],
+    children:[
+      { path: '', loadChildren: () => import('./modules/auth/home/home.module').then(m => m.HomeModule)},
+      { path: 'register', loadChildren: () => import('./modules/auth/register/register.module').then(m => m.RegisterModule)},
+    ]
+  },
 
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    children:[
+      { path: '', redirectTo: 'publications', pathMatch: 'full'},
+      { path: 'publications', loadChildren: () => import('./modules/admin/pages/publications/publications.module').then(m => m.PublicationsModule)},
+      { path: 'profile', loadChildren: () => import('./modules/admin/pages/profile/profile.module').then(m => m.ProfileModule)},
+      { path: 'meet', loadChildren: () => import('./modules/admin/pages/meet/meet.module').then(m => m.MeetModule)},
+      { path: 'messages', loadChildren: () => import('./modules/admin/pages/messages/messages.module').then(m => m.MessagesModule)},
+    ]
+  }
 ];
--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.verifyToken();
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.verifyToken();
   }
-  
+
+  verifyToken(): boolean{
+    if(localStorage.getItem('token')){
+      return true;
+    }
+    else{
+      this.router.navigate(['']);
+      return false;
+    }
+  }
+
 }
